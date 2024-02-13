d9d070ddf90f9bb2434932383d3902e24f1c00d1
Vaibhav Wagh <117511698+vaibhavwagh19@users.noreply.github.com>
2024-02-13 18:35:28 +0530

    Update 7_string1.py

--- day1/7_string1.py
+++ day1/7_string1.py
@@ -1,2 +1,28 @@
-a = "Hello, World!"
-print(a[1])
+
+# Assign String to a Variable
+a = "Jai shree ram!"
+print(a)
+
+
+# Multiline Strings
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+
+# Strings are Arrays
+print(a[1])    
+
+# Looping Through a String
+for x in "jai shreee ram!":
+  print(x)
+  
+# String Length
+print(len(a))
+
+# Check String
+print("ram" in a)
+
+# Check if NOT
+print("krish" not in a)
