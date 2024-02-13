a1e631ead98b90e34584a96173b89b762879e88d
Vaibhav Wagh <117511698+vaibhavwagh19@users.noreply.github.com>
2024-02-13 12:08:23 +0530

    Create 5_data_types.py

---
+++ day1/5_data_types.py
@@ -0,0 +1,58 @@
+# string
+x = "Hello World" 
+x = str("Hello World")
+
+#integer
+x=20
+x = int(20)
+
+# float
+x=20.5
+x = float(20.5)
+
+#complex
+x=1j
+x = complex(1j)
+
+# list
+x= ["luffy","zoro","sanji"]
+x = list(("apple", "banana", "cherry"))
+
+# tuple
+x= ("luffy","zoro","sanji")
+
+
+# dictionary
+x= {"luffy":1,"zoro":2,"sanji":3}
+x = tuple(("apple", "banana", "cherry"))
+
+# set
+x= {"luffy,","zoro","sanji"}
+x = set(("apple", "banana", "cherry"))
+
+# range
+x= range(6)
+x = range(6)
+
+# frozenset
+x= frozenset({"luffy","zoro","sanji"})
+x = frozenset(("apple", "banana", "cherry"))
+
+# boolean
+x= True
+x = bool(5)
+
+# bytes
+x= b"hello"
+x = bytes(5)
+
+# bytearray
+x=bytearray(5)
+x = bytearray(5)
+
+# memoryview
+x = memoryview(bytes(5))
+x = memoryview(bytes(5))
+
+# nonetype
+x= None
