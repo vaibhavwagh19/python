4ec36168483aa77f0b703d938a334a86c61fe2bb
Vaibhav Wagh <117511698+vaibhavwagh19@users.noreply.github.com>
2024-02-13 10:53:32 +0530

    Create variable2.py

---
+++ day1/variable2.py
@@ -0,0 +1,11 @@
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x,y,z)
+
+# x = y = z = "Orange"
+# print(x,y,z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
